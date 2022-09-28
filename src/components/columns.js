350e7aca61ff7438e694f6b082809d27cb49d2e6
fenny wilriani <fenny.wilriani@gmail.com>
2022-09-28 17:12:00 +0200

    adding files

---
+++ src/components/columns.js
@@ -0,0 +1,14 @@
+export const COLUMNS = [
+    {
+        Header: 'Id',
+        accessor: 'id'
+    },
+    {
+        Header: 'Genre', 
+        accessor: 'genres'
+    },
+    {
+        Header: 'Title',
+        accessor: 'titles'
+    }
+]
